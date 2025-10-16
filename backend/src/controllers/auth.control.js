c4d2c74f4ad3ac6c74e67ab3ce1ac7a1acbfb507
Gaurav SR <iamgauravsr@gmail.com>
2025-10-16 19:12:23 +0530

    add update profile endpoint

--- backend/src/controllers/auth.control.js
+++ backend/src/controllers/auth.control.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import User from '../models/user.model.js';
 import { generateToken } from '../lib/utils.js';
 import { sendWelcomeEmail } from '../emails/emailHandlers.js';
+import cloudinary from '../lib/cloudinary.js';
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -76,3 +77,21 @@ export const logout = (_, res) => {
   res.cookie('jwt', '', { maxAge: 0 });
   res.status(200).json({ message: "Logged out successfully" });
 }
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    if(!profilePic) {
+      return res.status(400).json({message: "Profile picture is required"});
+    }
+    const userId = req.userId;
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true }).select('-password');
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.log("Error in updateProfile controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
-import { signup, login, logout } from '../controllers/auth.control.js';
+import { signup, login, logout, updateProfile } from '../controllers/auth.control.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
@@ -9,5 +10,11 @@ router.post('/signup', signup);
 router.post('/login', login);  
 
 router.post('/logout', logout);
+
+router.put('/update-profile', protectRoute, updateProfile);
+
+router.get('/check', protectRoute, (_, res) => {
+  res.status(200).json({ message: "User is authenticated" });
+});
 
 export default router;
